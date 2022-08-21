66fb9b43d79b100bb2e599a8745bab0c0c378517
Viet Nguyen <vietnguyen.developermobile@gmail.com>
2022-08-21 14:51:08 +0700

    Add product list

--- back-end/routers/products.js
+++ back-end/routers/products.js
@@ -1,4 +1,5 @@
-const Product = require("../models/product");
+const { Product } = require("../models/product");
+const { Category } = require("../models/category");
 const express = require("express");
 const router = express.Router();
 
@@ -12,24 +13,42 @@ router.get(`/`, async (req, res) => {
   res.send(productList);
 });
 
-router.post(`/`, (req, res) => {
-  const product = new Product({
+router.post(`/`, async (req, res) => {
+  const category = await Category.findById(req.body.category);
+
+  if (!category) return res.status(400).send("Invalid Category");
+
+  let product = new Product({
     name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
     image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
     countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numberReviews: req.body.numberReviews,
+    isFeatured: req.body.isFeatured,
   });
 
-  product
-    .save()
-    .then((createProduct) => {
-      res.status(201).json(createProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
+  // product
+  //   .save()
+  //   .then((createProduct) => {
+  //     res.status(201).json(createProduct);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).json({
+  //       error: err,
+  //       success: false,
+  //     });
+  //   });
+
+  product = await product.save();
+
+  if (!product) return res.status(500).send("the product cannot be created!");
+
+  res.send(product);
 });
 
 module.exports = router;
