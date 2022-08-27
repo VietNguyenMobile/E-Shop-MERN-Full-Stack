57279a2692be0405aa57720a342851c1dafa08d5
Viet Nguyen <vietnguyen.developermobile@gmail.com>
2022-08-28 01:43:25 +0700

    Excluding REST API Routes From Authentication

--- back-end/helpers/jwt.js
+++ back-end/helpers/jwt.js
@@ -5,9 +5,18 @@ var { expressjwt: jwt } = require("express-jwt");
 
 function authJwt() {
   const secret = process.env.secret;
+  const api = process.env.API_URL;
   return jwt({
     secret,
     algorithms: ["HS256"],
+  }).unless({
+    path: [
+      // { url: `${api}/products`, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ],
   });
 }
 
